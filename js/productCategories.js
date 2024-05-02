389ace38ef68f8edc42708a37d4dadac2779ef79
Hoi Shan Lam <156137246+Halilam@users.noreply.github.com>
2024-05-02 14:11:41 -0400

    Project Start

---
+++ js/productCategories.js
@@ -0,0 +1,11 @@
+window.productCategories=[
+    {
+        name: "tools"
+    },
+    {
+        name: "containers"
+    },
+    {
+        name: "beans"
+    }
+]
---
+++ js/products.js
@@ -0,0 +1,92 @@
+window.products=[
+    {
+        productId: "001",
+        productName: "Product 1",
+        productDescription: "This is 001 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "tools",
+        availability: true
+    },
+    {
+        productId: "002",
+        productName: "Product 2",
+        productDescription: "This is 002 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "tools",
+        availability: true
+    },
+    {
+        productId: "003",
+        productName: "Product 3",
+        productDescription: "This is 003 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "tools",
+        availability: true
+    },
+    {
+        productId: "004",
+        productName: "Product 4",
+        productDescription: "This is 004 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "tools",
+        availability: true
+    },
+    {
+        productId: "005",
+        productName: "Product 5",
+        productDescription: "This is 005 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "tools",
+        availability: true
+    },
+    {
+        productId: "006",
+        productName: "Product 6",
+        productDescription: "This is 006 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "tools",
+        availability: true
+    },
+    {
+        productId: "007",
+        productName: "Product 7",
+        productDescription: "This is 007 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "containers",
+        availability: true
+    },
+    {
+        productId: "008",
+        productName: "Product 8",
+        productDescription: "This is 008 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "containers",
+        availability: true
+    },
+    {
+        productId: "009",
+        productName: "Product 9",
+        productDescription: "This is 009 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "containers",
+        availability: true
+    },
+    {
+        productId: "010",
+        productName: "Product 10",
+        productDescription: "This is 010 description.",
+        value: 10,
+        imageUrl: "https://na.cx/i/B2OpSpF.png",
+        category: "beans",
+        availability: true
+    }
+];
